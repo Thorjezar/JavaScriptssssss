4c03717812e4747a70a0adfa119df0dcf72328aa
YWM <thorjezar@example.com>
2019-03-25 12:06:13 +0800

    20190325上午

---
+++ 02运算符.js
@@ -0,0 +1,8 @@
+console.log(!true);
+console.log(!!!!true);
+
+// 非布尔值参与逻辑运算符
+console.log(34&&null); //null
+console.log(false&&null); //false
+console.log("welcome"&&null); //null
+console.log("welcome"||true); //"welcome"
